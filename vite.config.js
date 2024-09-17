526d0e7b92b6e3243931aee56bf58bc50cfad206
Daniel Pumarejo <104592674+DanyVaic18@users.noreply.github.com>
2024-09-17 02:06:37 -0500

    feat: package deploy and setting

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,6 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base:'https://danyvaic18.github.io/reto-tecnico-nelumbo'
+
 })
